4b5ad57914f26e36073d552e1f9ef386c75f1bca
contato@erick.net.br <contato@erick.net.br>
2017-10-15 12:08:51 -0300

    first commit

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-	"api/model"
 	"api/extra"
 )
 
 func main() {
-	b := model.Tag{"123", "2345"}
-	//rel := Tag
-	fmt.Println("starting", b.ToString)
-
+	fmt.Println("starting")
 	extra.Balance(123)
 }
--- views/tag.go
+++ views/tag.go
@@ -1,8 +1,14 @@
 package views
 
-import "net/http"
+import (
+	"net/http"
+	"encoding/json"
+	"api/model"
+)
 
-func GetTags(w http.ResponseWriter, r *http.Request) {}
+func GetTags(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(model.Tag{"123","Erick"})
+}
 func GetTag(w http.ResponseWriter, r *http.Request) {}
 func CreateTag(w http.ResponseWriter, r *http.Request) {}
 func DeleteTag(w http.ResponseWriter, r *http.Request) {}
