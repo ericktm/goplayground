019f00fddc2b57d68349debb35a8109837221fa3
contato@erick.net.br <contato@erick.net.br>
2018-02-17 21:14:19 -0300

    imports and exported values

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # goplayground
-test project to learn golang and test ideas
+
+Hi! This is not a serious project!
+
+It's just to learn golang and test ideas.
+I'm following the examples from Golang Tour. 
+
+https://tour.golang.org/welcome/1
--- views/tag.go
+++ views/tag.go
@@ -1,14 +1,14 @@
 package views
 
 import (
-	"net/http"
-	"encoding/json"
 	"api/model"
+	"encoding/json"
+	"net/http"
 )
 
 func GetTags(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(model.Tag{"123","Erick"})
+	json.NewEncoder(w).Encode(model.Tag{"123", "Erick"})
 }
-func GetTag(w http.ResponseWriter, r *http.Request) {}
+func GetTag(w http.ResponseWriter, r *http.Request)    {}
 func CreateTag(w http.ResponseWriter, r *http.Request) {}
 func DeleteTag(w http.ResponseWriter, r *http.Request) {}
