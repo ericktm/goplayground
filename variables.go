7586602c78fee8291133018946336a989c0f9e44
contato@erick.net.br <contato@erick.net.br>
2018-02-18 10:40:15 -0400

    variables

---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// declaring three variables of type boolean
+// if not defined, They assume the value false
+var c, python, java bool
+
+func main() {
+	// c = true
+	// if not defined, assume value Zero
+	var i int
+	fmt.Println(i, c, python, java)
+}
