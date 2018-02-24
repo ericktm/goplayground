4b1fe9f822e51abeacab7d00973101c7c31eab02
contato@erick.net.br <contato@erick.net.br>
2018-02-24 08:45:08 -0400

    type conversions

--- type-conversions.go
+++ type-conversions.go
@@ -8,7 +8,9 @@ import (
 func main() {
 	var x, y int = 3, 4
 	var f float64 = math.Sqrt(float64(x*x + y*y))
+
+	var k float64 = float64(x + y)
 	var z uint = uint(f)
-	fmt.Println(x, y, z, f)
+	fmt.Println(x, y, z, f, k)
 	fmt.Printf("%T %T %T %T\n", x, y, z, f)
 }
