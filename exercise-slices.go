1e53c5de8c523a03230b4674e22ad7c4154cba2e
contato@erick.net.br <contato@erick.net.br>
2018-04-01 10:38:29 -0400

    maps

---
+++ exercise-slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+
+	mypic := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		mypic[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			mypic[i][j] = uint8((j*2 - i) ^ i - j*15)
+		}
+	}
+	return mypic
+}
+
+func main() {
+	pic.Show(Pic)
+}
